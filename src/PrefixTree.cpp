edac2d03f0aa85f8466cb1ca7dfd0b395dfd1f9c
LouieSF <lsf2004impo@gmail.com>
2025-07-08 20:26:48 -0400

    adding prefix tree files

---
+++ src/PrefixTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lsf20 on 7/8/2025.
+//
+
+#include "PrefixTree.h"
---
+++ src/PrefixTree.h
@@ -0,0 +1,16 @@
+//
+// Created by lsf20 on 7/8/2025.
+//
+
+#ifndef PREFIXTREE_H
+#define PREFIXTREE_H
+
+
+
+class PrefixTree {
+
+};
+
+
+
+#endif //PREFIXTREE_H
